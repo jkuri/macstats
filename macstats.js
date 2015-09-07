0183c0c20dfd7910aebe0a0c1c19d64d00ce4dbd
Jan Kuri <jkuri88@gmail.com>
2015-09-07 02:39:00 +0200

    Initial commit

--- macstats.js
+++ macstats.js
@@ -1,3 +1,13 @@
+/** 
+
+macstatsJS
+
+Description: Node script for displaying you mac stats
+
+Author: Jan Kuri <jkuri88@gmail.com>
+Licence: MIT
+*/
+
 var cpu = require('./lib/cpu.js').cpu;
 	battery = require('./lib/battery.js').battery;
 
